d93c8ebbc9f3cbdbcc3e4293af22f5f4af86749b
Takashi Toyoshima <toyoshim@google.com>
2023-01-27 13:12:11 +0900

    Add version check

--- v3/main.js
+++ v3/main.js
@@ -1,5 +1,15 @@
 let currentStatus = null;
 
+function updateIcon(tabId, title, badgeText, badgeBgColor) {
+  chrome.action.setTitle({ tabId: tabId, title: title });
+  if (badgeText === undefined)
+    badgeText = '';
+  chrome.action.setBadgeText({ tabId: tabId, text: badgeText });
+  if (badgeBgColor) {
+    chrome.action.setBadgeBackgroundColor({ tabId: tabId, color: badgeBgColor });
+  }
+}
+
 function updateStatus(tabId, status) {
   currentStatus = status;
   if (!status)
@@ -30,9 +40,7 @@ function updateStatus(tabId, status) {
   }
   if (text === '|')
     text = '';
-  chrome.action.setBadgeText({ tabId: tabId, text: text });
-  chrome.action.setBadgeBackgroundColor({ tabId: tabId, color: color });
-  chrome.action.setTitle({ tabId: tabId, title: title });
+  updateIcon(tabId, title, text, color);
 }
 
 function checkPrerenderStatus(options) {
@@ -43,6 +51,7 @@ function checkPrerenderStatus(options) {
 
 function handleContentSwitch(options) {
   // update context menu rule.
+  console.log(options.url);
   const url = new URL(options.url);
   const portString = url.port ? (':' + url.port) : '';
   const sameOriginPattern = url.protocol + '//' + url.host + portString + '/*';
@@ -50,32 +59,54 @@ function handleContentSwitch(options) {
 }
 
 // Hooks
-chrome.tabs.onActivated.addListener(activeInfo => {
-  checkPrerenderStatus({ reason: 'onActivated', tabId: activeInfo.tabId, windowId: activeInfo.windowId });
-  chrome.tabs.get(activeInfo.tabId, tab => {
-    handleContentSwitch({ reason: 'onActivated', url: tab.url });
+function registerHooks() {
+  // Tab switch.
+  chrome.tabs.onActivated.addListener(activeInfo => {
+    checkPrerenderStatus({ reason: 'onActivated', tabId: activeInfo.tabId, windowId: activeInfo.windowId });
+    chrome.tabs.get(activeInfo.tabId, tab => {
+      handleContentSwitch({ reason: 'onActivated', url: tab.url });
+    });
   });
-});
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'loading' && changeInfo.url) {
-    handleContentSwitch({ reason: 'onUpdated.loading', url: changeInfo.url });
-  } else if (changeInfo.status === 'complete') {
-    checkPrerenderStatus({ reason: 'onUpdated.complete', tabId: tabId, windowId: tab.windowId });
-  }
-});
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message.message === 'update')
-    updateStatus(sender.tab.id, message.status);
-});
-
-// Context menus.
-chrome.contextMenus.create({
-  id: 'link',
-  contexts: ['link'],
-  title: 'Prerender this link'
-});
-chrome.contextMenus.onClicked.addListener(
-  (info, tab) => {
-    chrome.tabs.sendMessage(tab.id, { command: 'insertRule', url: info.linkUrl }, { frameId: 0 });
+  // Page load completion.
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'loading' && changeInfo.url) {
+      handleContentSwitch({ reason: 'onUpdated.loading', url: changeInfo.url });
+    } else if (changeInfo.status === 'complete') {
+      checkPrerenderStatus({ reason: 'onUpdated.complete', tabId: tabId, windowId: tab.windowId });
+    }
   });
+
+  // Request from content script.
+  chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+    if (message.message === 'update')
+      updateStatus(sender.tab.id, message.status);
+  });
+
+  // Context menus.
+  chrome.contextMenus.create({
+    id: 'link',
+    contexts: ['link'],
+    title: 'Prerender this link'
+  });
+  chrome.contextMenus.onClicked.addListener(
+    (info, tab) => {
+      chrome.tabs.sendMessage(tab.id, { command: 'insertRule', url: info.linkUrl }, { frameId: 0 });
+    });
+}
+
+let chromiumVersion = 110;
+for (let brand of navigator.userAgentData.brands) {
+  if (brand.brand != 'Chromium' && brand.brand != 'Google Chrome')
+    continue;
+  console.log('detect ' + brand.brand + ' version ' + brand.version);
+  chromiumVersion = brand.version;
+}
+if (chromiumVersion < 110) {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true},
+    tab => {
+      updateIcon(tab.id, 'Prerender Tweaks requires Chrome 110+', 'X', '#f00');
+    });
+} else {
+  registerHooks();
+}
