cb874de85d66309bf0217b3e165a0b0a2fd74101
Takashi Toyoshima <toyoshim@google.com>
2023-01-30 16:23:16 +0900

    Add LICENSE

--- v3/utils.js
+++ v3/utils.js
@@ -1,3 +1,7 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export function getChromiumVersion() {
     let chromiumVersion = 110;
     for (let brand of navigator.userAgentData.brands) {
