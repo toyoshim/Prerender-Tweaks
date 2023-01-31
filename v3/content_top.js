a0fe087c10d079b7a5e8882cbd8577796817d61d
Takashi Toyoshima <toyoshim@google.com>
2023-01-31 14:32:33 +0900

    clean up

--- v3/content_top.js
+++ v3/content_top.js
@@ -16,51 +16,13 @@ let prerenderStatus = {
 };
 let queried = false;
 let settings = chrome.runtime.sendMessage(undefined, { message: 'settings' });
+let prerenderedUrls = [];
 
 // Make the activated status up to date.
 if (document.prerendering) {
   document.addEventListener('prerenderingchange', () => {
     prerenderStatus.activated = true;
   });
-}
-
-async function getRemoteSetting(key) {
-  return (await settings)[key];
-}
-
-// Make the hasSpecrules status up to date, and notify the main.js on changes.
-function checkSpecrules(notify) {
-  if (prerenderStatus.hasSpecrules)
-    return;
-  prerenderStatus.hasSpecrules = false;
-  for (let script of document.getElementsByTagName('script')) {
-    if (script.type != 'speculationrules')
-      continue;
-    prerenderStatus.hasSpecrules = true;
-    break;
-  }
-  if (!notify || !queried || (prerenderStatus.prerendered && !prerenderStatus.activated))
-    return;
-  chrome.runtime.sendMessage(undefined, { message: 'update', status: prerenderStatus });
-}
-let mutationChecking = false;
-const mutationObserver = new MutationObserver(o => {
-  if (mutationChecking)
-    return;
-  mutationChecking = true;
-  setTimeout(e => {
-    mutationChecking = false;
-    checkSpecrules(true);
-  }, 100);
-});
-checkSpecrules(true);
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', e => {
-    checkSpecrules(true);
-    mutationObserver.observe(document, { childList: true, subtree: true });
-  });
-} else {
-  mutationObserver.observe(document, { childList: true, subtree: true });
 }
 
 // Make the restoredFromBFCache status up to date.
@@ -71,29 +33,62 @@ window.addEventListener('pageshow', e => {
   chrome.runtime.sendMessage(undefined, { message: 'update', status: prerenderStatus });
 });
 
-async function injectSpecrules(urls) {
-  if (!await getRemoteSetting('autoInjection')) {
+// Obtains Extensions settings delivered at the script start time.
+async function getRemoteSetting(key) {
+  return (await settings)[key];
+}
+
+// Make the hasSpecrules status up to date, and notify the main.js on changes.
+function checkSpecrules() {
+  if (prerenderStatus.hasSpecrules)
     return;
+  prerenderStatus.hasSpecrules = false;
+  for (let script of document.getElementsByTagName('script')) {
+    if (script.type != 'speculationrules')
+      continue;
+    prerenderStatus.hasSpecrules = true;
+    break;
   }
+  if (!queried || (prerenderStatus.prerendered && !prerenderStatus.activated))
+    return;
+  chrome.runtime.sendMessage(undefined, { message: 'update', status: prerenderStatus });
+}
+
+function scanContent() {
+  checkSpecrules();
+}
+
+// Inject speculationrules for specified URLs.
+async function injectSpecrules(urls) {
+  console.log('injecting speculationrules:');
   let rules = [];
   for (let url of urls) {
+    // Ignore if the url is already in the prerendered page list.
+    if (prerenderedUrls.indexOf(url) >= 0) {
+      continue;
+    }
+    prerenderedUrls.push(url);
     rules.push(`{ "source": "list", "urls": [ "${url}" ] }`);
+    console.log(' * ' + url);
+  }
+  if (rules.length == 0) {
+    return;
   }
   const rule = document.createElement('script');
   rule.type = 'speculationrules';
   rule.innerText = '{ "prerender": [ ' + rules.join(',') + ' ] }';
   document.head.appendChild(rule);
-  console.log('injecting speculationrules:');
-  for (let url of urls) {
-    console.log(' * ' + url);
-  }
   prerenderStatus.hasInjectedSpecrules = true;
 }
 
-// Inject a speculationrules for the first anchor tag on the load completion.
+// Inject a speculationrules for the first N anchor tag on the load completion.
 async function tryInjectingSpecrules() {
   if (prerenderStatus.hasSpecrules || prerenderStatus.hasInjectedSpecrules)
     return;
+
+  if (!await getRemoteSetting('autoInjection')) {
+    return;
+  }
 
   let urls = [];
   const maxRules = await getRemoteSetting('maxRulesByAnchors');
@@ -121,13 +116,37 @@ async function tryInjectingSpecrules() {
     return;
   injectSpecrules(urls);
 }
+
+// Content checks on load, and DOM modifications.
+let mutationChecking = false;
+const mutationObserver = new MutationObserver(o => {
+  if (mutationChecking)
+    return;
+  mutationChecking = true;
+  setTimeout(e => {
+    mutationChecking = false;
+    scanContent();
+  }, 100);
+});
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', e => {
+    scanContent();
+    mutationObserver.observe(document, { childList: true, subtree: true });
+  });
+} else {
+  scanContent();
+  mutationObserver.observe(document, { childList: true, subtree: true });
+}
+
+// Auto injection on page load completion.
 if (document.readyState === 'complete') {
   tryInjectingSpecrules();
+} else {
+  window.addEventListener('load', e => {
+    scanContent();
+    tryInjectingSpecrules();
+  });
 }
-window.addEventListener('load', e => {
-  checkSpecrules(false);
-  tryInjectingSpecrules();
-});
 
 // Communication with the main.js running in the background service worker.
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -135,7 +154,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     queried = true;
     sendResponse(prerenderStatus);
   } else if (message.command === 'insertRule') {
-    // TODO: Check if the rule for the url is already in the list.
     injectSpecrules([message.url]);
   }
 });
